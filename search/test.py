21bb9c3ff048e20fa4f75cafdcd02d5790959a76
alex-taxiera <alex.taxiera@gmail.com>
2018-02-20 13:56:30 -0500

    added Bing API test

---
+++ search/test.py
@@ -0,0 +1,8 @@
+from image import Search
+
+print(Search(2).getLinks("homeless man"))
+
+"""
+EXPECTED OUTCOME
+[u'https://cdn.pixabay.com/photo/2014/12/01/10/24/homeless-man-552571_640.jpg', u'https://cdn.pixabay.com/photo/2016/02/17/08/21/houmeles-1204653_640.jpg']
+"""
