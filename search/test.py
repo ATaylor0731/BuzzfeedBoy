5dd5bec2cd007ff36cd7bc4e3575c3f6637c914f
alex-taxiera <alex.taxiera@gmail.com>
2018-02-20 15:38:43 -0500

    updated test suite

--- search/test.py
+++ search/test.py
@@ -1,8 +1,12 @@
 from image import Search
 
-print(Search(2).getLinks("homeless man"))
+print(Search(1).getLinks("homeless man"))
+print(Search(1).getThumbs("homeless man"))
+print(Search(1).getLinksAndThumbs("homeless man"))
 
 """
 EXPECTED OUTCOME
-[u'https://cdn.pixabay.com/photo/2014/12/01/10/24/homeless-man-552571_640.jpg', u'https://cdn.pixabay.com/photo/2016/02/17/08/21/houmeles-1204653_640.jpg']
+[u'https://cdn.pixabay.com/photo/2014/12/01/10/24/homeless-man-552571_640.jpg']
+[u'https://tse2.mm.bing.net/th?id=OIP.-26O8GcnriZiVHHl0P9SMgHaFj&pid=Api']
+[Image(thumb=u'https://tse2.mm.bing.net/th?id=OIP.-26O8GcnriZiVHHl0P9SMgHaFj&pid=Api', link=u'https://cdn.pixabay.com/photo/2014/12/01/10/24/homeless-man-552571_640.jpg')]
 """
