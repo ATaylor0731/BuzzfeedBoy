408bec14d8334a7cb1289bd41a461c2b89a78f86
ATaylor0731 <anthony.taylor@mymail.champlain.edu>
2018-02-22 22:16:33 -0500

    First iteration of title and image generation posting to a new HTML page.

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ def index():
     search=(Search(1).getThumbs("homeless man"))
     return render_template("main.html", picture=search[0])
 
+@app.route('/searchImage', methods=['POST', 'GET'])
+def searchImages():
+    if request.method == 'POST':
+        words = request.form
+        clickbaitTitle = "You won't believe what this " + words['ajv1'] + " " + words['noun1'] + " does next!!!"
+        
+        search = (Search(5).getThumbs(words['ajv1'] + " " + words['noun1']))
+        
+        return render_template("answer.html", title = clickbaitTitle, picture1=search[0], picture2=search[1], picture3=search[2], picture4=search[3], picture5=search[4])
+    
 if __name__ == "__main__":
     app.run(debug=True)
